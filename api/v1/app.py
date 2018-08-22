1c65c44ae228d911dc0839c979b98ff67e0bdfc7
AmyLGalles <suzyhendrix@gmail.com>
2018-08-22 18:09:20 +0000

    adding 404 handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,8 +10,15 @@ app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
 @app.teardown_appcontext
-def contextclose():
+def teardown(self):
     storage.close()
 
 if __name__=="__main__":
     app.run(host='0.0.0.0', port=5000, threaded=True)
+
+@app.errorhandler(404)
+'''
+Handler for 404 errors that returns a JSON-formatted 404 status code response.
+'''
+def page_not_found():
+return make_response(jsonify({'error': 'Not Found'}), 404)
