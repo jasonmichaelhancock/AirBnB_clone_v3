5ecf5ac2d67471411b0f35ba9135650097a4375b
AmyLGalles <suzyhendrix@gmail.com>
2018-08-22 19:26:51 +0000

    argument errors were keeping the error handler from responding correctly

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,9 +2,9 @@
 '''
     API for HolbertonBnB
 '''
-from flask import Flask
 from api.v1.views import app_views
 from models import storage
+from flask import Flask, make_response, jsonify
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
@@ -14,7 +14,7 @@ def teardown(self):
     storage.close()
 
 @app.errorhandler(404)
-def page_not_found():
+def page_not_found(error):
     return make_response(jsonify({'error': 'Not Found'}), 404)
 
 if __name__=="__main__":
