2dbf7eb222a3cee98635db97fc88131cae521073
AmyLGalles <suzyhendrix@gmail.com>
2018-08-22 07:18:05 +0000

    partial success on returning count, printing number only

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,11 @@
 from api.v1.views import app_views
+from models import storage
+from flask import jsonify
 
 @app_views.route('/status')
 def status():
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def stats():
+    return jsonify(storage.count())
