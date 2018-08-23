11745948873d73f7fa8cb127f4320191735af479
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-08-23 22:27:23 +0000

    task5 pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,6 +9,7 @@ from flask import jsonify
 from os import getenv
 import models
 
+
 @app_views.route('/status')
 def status():
     '''
@@ -16,15 +17,16 @@ def status():
     '''
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/stats')
 def stats():
-   '''Returns number of each type of object.'''
-   stats_list = {
-       'amenities': storage.count('Amenity'),
-       'cities': storage.count('City'),
-       'places': storage.count('Place'),
-       'reviews': storage.count('Review'),
-       'states': storage.count('State'),
-       'users': storage.count('User')
-   }
-   return jsonify(stats_list)
+    '''Returns number of each type of object.'''
+    stats_list = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats_list)
