d0caa16b594e2bb21e5a401036b2b66d6881994e
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-08-23 20:56:23 +0000

    task4 revise app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,18 +8,21 @@ from flask import Flask, make_response, jsonify
 from os import getenv
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views, url_prefix='/api/v1')
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 @app.teardown_appcontext
-def teardown(self):
+def teardown(exception):
+    """Teardown and close"""
     storage.close()
+
 
 @app.errorhandler(404)
 def page_not_found(error):
+    """Return error status"""
     return make_response(jsonify({'error': 'Not Found'}), 404)
 
 if __name__=="__main__":
-    host = os.getenv('HBNB_API_HOST', default='0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', default='5000'))
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', default=5000))
     app.run(host, int(port), threaded=True)
