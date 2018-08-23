b122a42ff6b8e877ecdfa448c4b4466a887656d6
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-08-23 07:22:36 +0000

    task4 revise port/host

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,10 +5,12 @@
 from api.v1.views import app_views
 from models import storage
 from flask import Flask, make_response, jsonify
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
 @app.teardown_appcontext
 def teardown(self):
     storage.close()
@@ -18,4 +20,10 @@ def page_not_found(error):
     return make_response(jsonify({'error': 'Not Found'}), 404)
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    host = os.getenv('HBNB_API_HOST')
+    if host is None:
+        host = '0.0.0.0'
+    port = os.getenv('HBNB_API_PORT')
+    if port is None:
+        port = 5000
+    app.run(host, int(port), threaded=True)
