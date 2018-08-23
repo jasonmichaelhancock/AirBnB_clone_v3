dd657b5af653aa104f5c56b3caaeb5041c28ada4
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-08-23 02:09:13 +0000

    change branch

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,10 @@
+#!/usr/bin/python3
+'''
+Module element.
+'''
+
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__)
+app_views = Blueprint('app_views', __name__, url_prefix='api/v1')
 
 from api.v1.views.index import *
